7ef8ef36002628b210a55932263b8357600c5fb8
Staiger, R. (Ryan) <ryansta@nedbank.co.za>
2020-03-31 17:32:05 +0200

    Updated 5319 and 5019 specs with DCAR details. Added a util to help people.

---
+++ JavaProductCatalogueServiceComponents/src/za/co/nb/productcatalogue/exception/InvalidAttributeException.java
@@ -0,0 +1,11 @@
+package za.co.nb.productcatalogue.exception;
+
+public class InvalidAttributeException extends Exception {
+
+	private static final long serialVersionUID = -1983110150950523882L;
+
+	public InvalidAttributeException(String message) {
+		super(message);
+	}
+	
+}
---
+++ JavaProductCatalogueServiceComponents/src/za/co/nb/productcatalogue/util/ProductSpecificationUtil.java
@@ -0,0 +1,87 @@
+package za.co.nb.productcatalogue.util;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import za.co.nb.productcatalogue.dao.ProductSpecificationsServiceDAO;
+import za.co.nb.productcatalogue.exception.InvalidAttributeException;
+import za.co.nednet.it.contracts.services.ent.productandservicedevelopment.channelproductcatalogue.v1.FeatureAttributeGroupType;
+import za.co.nednet.it.contracts.services.ent.productandservicedevelopment.channelproductcatalogue.v1.FeatureAttributesType;
+import za.co.nednet.it.contracts.services.ent.productandservicedevelopment.channelproductcatalogue.v1.FeaturesType;
+import za.co.nednet.it.contracts.services.ent.productandservicedevelopment.channelproductcatalogue.v1.ProductAttributeGroupType;
+import za.co.nednet.it.contracts.services.ent.productandservicedevelopment.channelproductcatalogue.v1.ProductType;
+import za.co.nednet.it.contracts.services.ent.productandservicedevelopment.channelproductcatalogue.v1.ProductattributesType;
+
+public class ProductSpecificationUtil {
+
+	private final Log mLog = LogFactory.getLog(getClass());
+
+	public String getProductOrFeatureAttributeValue(String productID, String attributeGroup, String attributeName) throws Exception {
+		mLog.debug("Trace 1");
+		
+		// First get the product spec.
+		ProductSpecificationsServiceDAO dao = new ProductSpecificationsServiceDAO();
+		ProductType productSpecification = dao.getProductSpecificationXMLByID(productID);
+
+		mLog.debug("Trace 2");
+		
+		return getProductOrFeatureAttributeValue(productSpecification, attributeGroup, attributeName);
+	}
+
+	public String getProductOrFeatureAttributeValue(ProductType productSpecification, String attributeGroup, String attributeName) throws Exception {
+		mLog.debug("Trace 1");
+		
+		// First look in product attribute groups.
+		for(ProductAttributeGroupType productAttributeGroup : productSpecification.getProductAttributeGroup()) {
+			mLog.debug("Trace 2");
+			
+			// Now look for the attribute group.
+			if(productAttributeGroup.getAttributeGroupName().equalsIgnoreCase(attributeGroup)) {
+				mLog.debug("Trace 3");
+				// Found it.
+				// Now find the attribute.
+				for(ProductattributesType attribute : productAttributeGroup.getProductAttributes()) {
+					mLog.debug("Trace 4");
+
+					if(attribute.getAttributeName().equalsIgnoreCase(attributeName)) {
+						// Found it.
+						mLog.debug("Trace 5");
+						
+						return attribute.getValue();
+					}
+				}
+			}
+		}
+		
+		// Didn't find it on product level. Look in features.
+		for(FeaturesType feature : productSpecification.getFeatures()) {
+			mLog.debug("Trace 6");
+
+			for(FeatureAttributeGroupType featureAttributeGroup : feature.getFeatureAttributeGroup()) {
+				mLog.debug("Trace 7");
+				
+				// Now look for the attribute group.
+				if(featureAttributeGroup.getAttributeGroupName().equalsIgnoreCase(attributeGroup)) {
+					mLog.debug("Trace 8");
+					// Found it.
+					// Now find the attribute.
+					for(FeatureAttributesType attribute : featureAttributeGroup.getFeatureAttributes()) {
+						mLog.debug("Trace 9");
+
+						if(attribute.getAttributeName().equalsIgnoreCase(attributeName)) {
+							// Found it.
+							mLog.debug("Trace 10");
+							
+							return attribute.getValue();
+						}
+					}
+				}
+			}
+		}
+		
+		mLog.debug("Trace 11");
+		
+		// Didn't find the attribute.
+		throw new InvalidAttributeException("Invalid product specification attribute for product ID " + productSpecification.getProductIdentifier() + " with attribute group " + attributeGroup + " and attribute name " + attributeName);
+	}
+}
