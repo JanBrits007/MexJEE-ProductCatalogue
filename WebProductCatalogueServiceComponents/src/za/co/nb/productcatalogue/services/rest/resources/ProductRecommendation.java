8849daae80b79d2c9ecd054fb7a8ecc98e853c55
NB320512 <thapeloc@nedbank.co.za>
2020-08-05 12:46:13 +0200

    UID9009-9393 Expose new API

---
+++ WebProductCatalogueServiceComponents/src/za/co/nb/productcatalogue/services/rest/resources/ProductRecommendation.java
@@ -0,0 +1,17 @@
+package za.co.nb.productcatalogue.services.rest.resources;
+
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import javax.ejb.Stateless;
+import javax.ws.rs.Path;
+
+@Path("/productandservicedevelopment/productrecommendations/v1")
+@Stateless
+public class ProductRecommendation {
+    private final Log mLog = LogFactory.getLog(getClass());
+
+
+
+}
