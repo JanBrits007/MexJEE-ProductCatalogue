3d79302da755b7d6ec3a92788be6c3db4bc10f98
CC308394 <ChristiaanPr@nedbank.co.za>
2023-07-11 11:52:41 +0200

    New entitlements config for REST as well

--- WebProductCatalogueServiceComponents/src/main/java/za/co/nb/productcatalogue/services/rest/applications/ProductConfig.java
+++ WebProductCatalogueServiceComponents/src/main/java/za/co/nb/productcatalogue/services/rest/applications/ProductConfig.java
@@ -3,11 +3,21 @@ package za.co.nb.productcatalogue.services.rest.applications;
 import za.co.nb.productcatalogue.services.rest.resources.ProductCatalogueResource;
 import za.co.nb.productcatalogue.services.rest.resources.ProductRecommendationResource;
 import za.co.nb.productcatalogue.services.rest.resources.ProductSpecificationsResource;
+import za.co.nb.ws.soap.handler.EntitlementsLoader;
+import za.co.nedbank.entitlement.transport.EntitlementsAuthenticationPathFilter;
 
 import java.util.HashSet;
 import java.util.Set;
 import javax.ws.rs.core.Application;
 public class ProductConfig extends Application {
+	@Override
+	public Set<Object> getSingletons()
+	{
+		Set<Object> singletons = new HashSet<>();
+		singletons.add(new EntitlementsAuthenticationPathFilter(EntitlementsLoader.getLocalJndiName("SysProductCatalogueServiceComponents","WebProductCatalogueServiceComponents")));
+		// register others, if any
+		return singletons;
+	}
 	  @Override
 	  	   public Set<Class<?>> getClasses() {
 	        Set<Class<?>> classes = new HashSet<Class<?>>();
